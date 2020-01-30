5599d59855ef9bb2b03d96921169e0af51b25e60
Willian Rodelo <wrodelogarcia@gmail.com>
2020-01-29 22:56:55 -0500

    Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -16,7 +16,7 @@ import ScrollToBottom from "react-scroll-to-bottom";
 import io from "socket.io-client";
 
 
-const socket = io("http://localhost:1337/");
+const socket = io("https://server-chat-service.herokuapp.com/");
 
 
 const useStyles = makeStyles(theme => ({
