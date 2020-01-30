ebfeabf7cab271eb69ea86d311203775d6d98138
Willian Rodelo <wrodelogarcia@gmail.com>
2020-01-29 23:25:01 -0500

    Chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "../App.css";
 import TextField from "@material-ui/core/TextField";
 import List from "@material-ui/core/List";
 import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
+
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import { useHistory,useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import ScrollToBottom from "react-scroll-to-bottom";
 
 import io from "socket.io-client";
 
-
 const socket = io("https://server-chat-service.herokuapp.com/");
-
+//const socket = io("http://localhost:1337/");
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,7 +32,6 @@ const useStyles = makeStyles(theme => ({
 function Chat() {
   let location = useLocation();
 
-
   const classes = useStyles();
   const [message, setMessage] = useState({
     data: []
@@ -44,7 +42,6 @@ function Chat() {
   });
 
   const getValue = e => {
-    
     const name = e.target.name;
     const value = e.target.value;
     setTxtMessage({
@@ -53,18 +50,25 @@ function Chat() {
     });
   };
 
+  const [countUsers, setCountUsers] = useState({
+    count: 0
+  });
+
   var i = 0;
   const sendMessage = e => {
-    i++
+    i++;
     if (e.key === "Enter") {
-      console.log(i)
+      console.log(i);
       console.log(e.target.value);
       // Do code here
-      console.log('enter')
+      console.log("enter");
       var txtmessage = e.target.value;
       if (txtmessage !== "") {
-        console.log(location.state.username)
-        socket.emit("message", { user: location.state.username, message: txtmessage });
+        console.log(location.state.username);
+        socket.emit("message", {
+          user: location.state.username,
+          message: txtmessage
+        });
         //var join = message.data.concat({ user: "Test", message: txtmessage });
         /*setMessage({
           data: join
@@ -76,8 +80,22 @@ function Chat() {
     }
   };
 
+  useEffect(()=>{
+    socket.on("usersonline", count => {
+      console.log("Cambia")
+      var countUser = count - 1;
+      console.log('user',countUser)
+      setCountUsers({
+        count: countUser
+      });
+    });
+  })
+
   socket.on("sendsmessages", socket => {
-    var join = message.data.concat({ user: socket.user, message: socket.message });
+    var join = message.data.concat({
+      user: socket.user,
+      message: socket.message
+    });
     setMessage({
       data: join
     });
@@ -86,7 +104,7 @@ function Chat() {
   return (
     <div className="App">
       <h1>Chat</h1>
-
+      
       <div className="chat">
         <div className="chat-content">
           <ScrollToBottom className="chat-message">
